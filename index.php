aaca336d3762cd44b8730c5f9c80e6d7f41d068f
osmank3 <osmank3@gmail.com>
2011-02-11 01:09:24 +0200

    ilk gönderim

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Fen Bilgisi</title>
+</head>
+<?php
+/*if (!logined) {
+include "login.php"
+}
+elseif {*/
+?>
+<body>
+bu sayfada ilgili şeyler gösterilecek
+</body>
+<?php// } ?>
+</html>
