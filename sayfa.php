54ed5e7c194fe9f52de92fc37c18bd8faef1002f
osmank3 <osmank3@gmail.com>
2011-04-20 19:31:34 +0000

    profil devamına gelen içerik hatası düzeltildi.

--- sayfa.php
+++ sayfa.php
@@ -1,40 +1,49 @@
-<?php 
-switch ($_GET["hesap"])
-{
-    case "cikis":
-        unset($_SESSION["giris"]);
-        unset($_SESSION["kullanici"]);
-        unset($_SESSION["kid"]);
-        echo "<script> window.top.location = './'; </script>";
-        break;
-        
-    case "goster":
-        echo "<h3>Profil</h3>";
-        if ($_GET["id"]) profilTablola($_GET["id"]);
-        else profilTablola($_SESSION["kid"]);
-        break;
-    case "ayarla":
-        $bicim = $_GET["bicim"];
-        include "profil.php";
-}
-switch ($_GET["sil"])
-{
-    case "icerik":
-        $sorgu = "UPDATE {$dbOnek}icerik SET goster='False' WHERE id=$_GET[sil_id]";
-        mysql_query($sorgu, $db);
-        unset($_POST);
-        echo "<script> document.location.href = '$_SERVER[HTTP_REFERER]' </script>";
-        break;
-    case "yorum":
-        unset($_POST);
-        $sorgu = "UPDATE {$dbOnek}yorum SET goster='False' WHERE id=$_GET[sil_id]";
-        mysql_query($sorgu, $db);
-        echo "<script> document.location.href = '$_SERVER[HTTP_REFERER]' </script>";
-        break;
-}
+<?php
 if ($_GET)
 {
-    include "icerik.php";
+    if ($_GET["hesap"])
+    {
+        switch ($_GET["hesap"])
+        {
+            case "cikis":
+                unset($_SESSION["giris"]);
+                unset($_SESSION["kullanici"]);
+                unset($_SESSION["kid"]);
+                echo "<script> window.top.location = './'; </script>";
+                break;
+                
+            case "goster":
+                echo "<h3>Profil</h3>";
+                if ($_GET["id"]) profilTablola($_GET["id"]);
+                else profilTablola($_SESSION["kid"]);
+                break;
+            case "ayarla":
+                $bicim = $_GET["bicim"];
+                include "profil.php";
+        }
+    }
+    elseif ($_GET["sil"])
+    {
+        switch ($_GET["sil"])
+        {
+            case "icerik":
+                $sorgu = "UPDATE {$dbOnek}icerik SET goster='False' WHERE id=$_GET[sil_id]";
+                mysql_query($sorgu, $db);
+                unset($_POST);
+                echo "<script> document.location.href = '$_SERVER[HTTP_REFERER]' </script>";
+                break;
+            case "yorum":
+                unset($_POST);
+                $sorgu = "UPDATE {$dbOnek}yorum SET goster='False' WHERE id=$_GET[sil_id]";
+                mysql_query($sorgu, $db);
+                echo "<script> document.location.href = '$_SERVER[HTTP_REFERER]' </script>";
+                break;
+        }
+    }
+    else
+    {
+        include "icerik.php";
+    }
 }
 else
 {
